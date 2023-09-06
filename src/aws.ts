ea5961090005a1bf603e9261e2ea117303ce8b9f
Rei <skylarnguyen25@gmail.com>
2023-09-06 15:35:29 +0700

    chore: update dynamodb entity

--- src/aws.ts
+++ src/aws.ts
@@ -1,5 +1,8 @@
 import AWS from "aws-sdk";
 import { AWSRegion } from "./types/aws";
+import { Vendor } from "./types/twitter";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import vendors from "./data/vendos";
 
 AWS.config.update({ region: AWSRegion.AP_SOUTHEAST_1 });
 
@@ -19,5 +22,66 @@ export const dynamodbCreateTable = async (
     }
 
     throw new Error("dynamodbCreateTable error object unknown type");
+  }
+};
+
+export const dynamodbDescribeTable = async (tableName: string) => {
+  try {
+    const table = await dynamodb
+      .describeTable({
+        TableName: tableName,
+      })
+      .promise();
+
+    console.log("Table retrieved", table);
+    return table;
+  } catch (e) {
+    if (e instanceof Error) {
+      return e;
+    }
+
+    throw new Error(`dynamodbDescribeTable error object unknown type`);
+  }
+};
+
+export const dynamodbDeleteTable = async (tableName: string) => {
+  try {
+    const result = await dynamodb
+      .deleteTable({
+        TableName: tableName,
+      })
+      .promise();
+
+    console.log("Table deleted", result);
+    return result;
+  } catch (e) {
+    if (e instanceof Error) {
+      return e;
+    }
+
+    throw new Error(`dynamodbDeleteTable error object unknown type`);
+  }
+};
+
+export const dynamodbCreateRecord = async (
+  tableName: string,
+  vendor: Vendor
+) => {
+  try {
+    const results = await dynamodb
+      .putItem({
+        TableName: tableName,
+        Item: marshall(vendor),
+      })
+      .promise();
+
+    console.log("Record created");
+    return results;
+  } catch (e) {
+    if (e instanceof Error) {
+      return e;
+    }
+
+    throw new Error(`dynamodbDeleteTable error object unknown type`);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { dynamodbDescribeTable, dynamodbCreateTable } from "./aws";
+import {
+  dynamodbDescribeTable,
+  dynamodbCreateTable,
+  dynamodbDeleteTable,
+  dynamodbCreateRecord,
+} from "./aws";
+import vendors from "./data/vendos";
+
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const init = async () => {
   const vendorsTablleName = "vendors";
@@ -23,8 +31,23 @@ const init = async () => {
   };
 
   const vendorsTable = await dynamodbDescribeTable(vendorsTablleName);
-  if (vendorsTable) {
+  if (!(vendorsTable instanceof Error)) {
+    // DELETE THE TABLE
+    console.log("Triggered");
+    await dynamodbDeleteTable(vendorsTablleName);
+    await sleep(6000);
   }
 
-  //   await dynamodbCreateTable(vendorsTablleParams);
+  await dynamodbCreateTable(vendorsTablleParams);
+  await sleep(6000);
+
+  for (const i in vendors) {
+    const vendor = vendors[i];
+    const res = await dynamodbCreateRecord(vendorsTablleName, vendor);
+    if (res instanceof Error) {
+      console.log("Error:", vendor, res);
+    }
+  }
 };
+
+init();
